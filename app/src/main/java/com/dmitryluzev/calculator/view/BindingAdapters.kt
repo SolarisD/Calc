c1cb93a1271212d86722de0554d24c3adbf216a1
Dmitry Luzev <luzevdm@yandex.ru>
2020-12-11 13:16:48 +0400

    Fix operation display

--- app/src/main/java/com/dmitryluzev/calculator/view/BindingAdapters.kt
+++ app/src/main/java/com/dmitryluzev/calculator/view/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.dmitryluzev.calculator.view
 
+import android.view.View
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -32,6 +33,33 @@ fun bindOperationTag(textView: TextView, operation: Operation?){
         is Multiply -> textView.text = textView.context.getString(R.string.multiply_tag)
         is Divide -> textView.text = textView.context.getString(R.string.divide_tag)
         is Percent -> textView.text = textView.context.getString(R.string.percent_tag)
+    }
+}
+
+@BindingAdapter("bindOperation")
+fun bindOperation(textView: TextView, operation: Operation?){
+    textView.visibility = if (operation != null && operation.result() == null) View.VISIBLE else View.GONE
+    
+    when(operation){
+        is BinaryOperation -> {
+            val tag = when(operation){
+                is Add -> textView.context.getString(R.string.add_tag)
+                is Subtract -> textView.context.getString(R.string.subtract_tag)
+                is Multiply -> textView.context.getString(R.string.multiply_tag)
+                is Divide -> textView.context.getString(R.string.divide_tag)
+                is Percent -> textView.context.getString(R.string.percent_tag)
+                else -> ""
+            }
+            val text = StringBuilder()
+            operation.a?.let {
+                text.append(Converter.doubleToString(it))
+                text.append(tag)
+            }
+            operation.b?.let { text.append(Converter.doubleToString(it)) }
+            operation.result()?.let { text.append(" = "); text.append(Converter.doubleToString(it)) }
+
+            textView.text = text.toString()
+        }
     }
 }
 
